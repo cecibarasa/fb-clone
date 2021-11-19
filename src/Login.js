695e2116f186bba75e2ad8f58c6edaa6d282401a
Cecilia Barasa <you@example.com>
2021-11-19 10:49:24 +0300

    updated login with google functionality

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import "./Login.css"
 import { Button } from '@mui/material'
-import { auth, provider } from "./firebase"
+import { auth, provider } from "./firebase";
+// import provider from "./firebase";
 
 
 function Login() {
@@ -19,7 +20,7 @@ function Login() {
             <div className="login__logo">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/1200px-2021_Facebook_icon.svg.png" alt="" />
                 <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt="" />
-                <Button type="submit" onClick={signIn}>
+                <Button className="button" type="submit" onClick={signIn}>
                     Sign In
                 </Button>
             </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,28 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase";
-// // import firebase from 'firebase/compat/app';
-// import "firebase/compat/auth"
-// import "firebase/compat/firestore"
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import firebase from "firebase";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzPOZHYxXuWQV0rnSgw8gwoh3OGUwOUiQ",
-    authDomain: "facebook-clone-6d2f2.firebaseapp.com",
-    projectId: "facebook-clone-6d2f2",
-    storageBucket: "facebook-clone-6d2f2.appspot.com",
-    messagingSenderId: "85790553435",
-    appId: "1:85790553435:web:ffba520f9668dc38d0967e",
-    measurementId: "G-RC1H83W3DK"
-  };
+  apiKey: "AIzaSyDyKshUluLxrqAJgDs6FQB8l6QA5kQ7D9g",
+  authDomain: "facebook--clone-3a4e6.firebaseapp.com",
+  projectId: "facebook--clone-3a4e6",
+  storageBucket: "facebook--clone-3a4e6.appspot.com",
+  messagingSenderId: "959761861508",
+  appId: "1:959761861508:web:a94cb4f32f4ae899fcebd1",
+  measurementId: "G-FJNKP50C9B"
+};
+// firebase.initializeApp(firebaseConfig);
+
+// export const auth = firebase.auth();
+
+// const provider = new firebase.auth.GoogleAuthProvider();
+// provider.setCustomParameters({ prompt: 'select_account' });
+// export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+// export default firebase;
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
